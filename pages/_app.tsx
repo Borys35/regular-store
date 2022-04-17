d4a439f420ec4abc1122d02a871efb8610ca8c8d
Borys35 <boryskac10@gmail.com>
2022-04-17 19:35:12 +0200

    Added progress bar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,17 @@
 import type { AppContext, AppProps } from "next/app";
 import App from "next/app";
+import { Router } from "next/router";
+import NProgress from "nprogress";
 import { Provider } from "react-redux";
 import { commerce } from "../lib/commerce";
 import { createStore } from "../store";
 import "../styles/globals.css";
+import "../styles/nprogress.css";
+
+NProgress.configure({ showSpinner: false });
+Router.events.on("routeChangeStart", NProgress.start);
+Router.events.on("routeChangeError", NProgress.done);
+Router.events.on("routeChangeComplete", NProgress.done);
 
 function MyApp({ Component, pageProps }: AppProps) {
   const { merchant, categories } = pageProps;
