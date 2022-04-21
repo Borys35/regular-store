c81235d1c97feadc515a5928f1c5438ce7dbe326
Borys35 <boryskac10@gmail.com>
2022-04-21 21:11:37 +0200

    Added cart component

--- components/common/header/header.tsx
+++ components/common/header/header.tsx
@@ -3,13 +3,16 @@ import { FC } from "react";
 import { FaShoppingCart, FaUser } from "react-icons/fa";
 import { useAppSelector } from "../../../hooks/useAppSelector";
 import { useRetrieveCartQuery } from "../../../store/apis/cart";
+import Button from "../../atoms/button";
 import Heading from "../../atoms/heading";
+import CartItem from "../../cart/cart-item";
+import HeaderIcon from "./header-icon";
 import HeaderNav from "./header-nav";
 import HeaderSearch from "./header-search";
 
 const Header: FC = () => {
   const merchant = useAppSelector((state) => state.merchant);
-  const { data } = useRetrieveCartQuery();
+  const { data: cart } = useRetrieveCartQuery();
 
   return (
     <header className="flex flex-col gap-6 pt-6">
@@ -20,10 +23,41 @@ const Header: FC = () => {
           </Link>
         </Heading>
         <HeaderSearch />
-        <div className="flex gap-6">
-          <FaShoppingCart size={24} className="fill-gray-600" />
-          <FaUser size={24} className="fill-gray-600" />
-          {data?.total_items}
+        <div className="flex items-center gap-6">
+          <HeaderIcon
+            iconComponent={() => <FaUser size={18} className="fill-gray-600" />}
+          />
+          <span className="group relative">
+            <HeaderIcon
+              iconComponent={() => (
+                <Link href="/cart">
+                  <a>
+                    <FaShoppingCart size={18} className="fill-gray-600" />
+                  </a>
+                </Link>
+              )}
+              counter={cart?.total_unique_items}
+            />
+            {cart && (
+              <div
+                className={`hidden absolute top-full text-center
+                            translate-y-1 px-3 pt-2 pb-4 border-1 border-accent 
+                            rounded bg-white w-80 z-20
+                            md:flex flex-col gap-2 transition-transform origin-top-right
+                            shadow-md right-0 scale-0 group-hover:scale-100`}
+              >
+                <Heading level={3}>Cart</Heading>
+                <div className="flex flex-col gap-2 mb-2">
+                  {cart.line_items.map((lineItem) => (
+                    <CartItem key={lineItem.id} item={lineItem} />
+                  ))}
+                </div>
+                <Button variant="primary" to="/cart">
+                  Cart
+                </Button>
+              </div>
+            )}
+          </span>
         </div>
       </div>
       <div className="border-t-1 border-accent"></div>
